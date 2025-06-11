fbd9e6ceb8fcca95b42593e8ad9cab9fb185eb3e
Leysan@LAPTOP-25B9S5T6 <leisan.nasretdinova@gmail.com>
2025-06-11 17:57:52 +0200

    esercizio 4

--- index.php
+++ index.php
@@ -29,22 +29,22 @@ for ($i = 1; $i <= 30; $i++) {
 
 */
 $arrays = [
-  [
-    'name' => 'Gianbertoldo',
-    'gender' => 'M',
-  ],
-  [
-    'name' => 'Lucrezia',
-    'gender' => 'F',
-  ],
-  [
-    'name' => 'Pancrazio',
-    'gender' => 'M',
-  ],
-  [
-    'name' => 'Mari*',
-    'gender' => '',
-  ],
+    [
+        'name' => 'Gianbertoldo',
+        'gender' => 'M',
+    ],
+    [
+        'name' => 'Lucrezia',
+        'gender' => 'F',
+    ],
+    [
+        'name' => 'Pancrazio',
+        'gender' => 'M',
+    ],
+    [
+        'name' => 'Mari*',
+        'gender' => '',
+    ],
 ];
 
 for ($i = 0; $i < count($arrays); $i++) {
@@ -115,6 +115,184 @@ while ($budget > 0 && $i < count($articoli)) {
 
 if ($budget === 0) {
     echo "Budget esaurito.\n";
-} 
+}
+
+
+//---- Esercizi con funzioni ----
+
+//---- Esercizio  1 ----
+/*Utilizzando un approccio funzionale (piccole funzioni che risolvono un piccolo problema), 
+riscrivere il programma fatto in precedenza che stampi in console tutti i numeri da 1 a 30.
+
+    Se il numero e’ multiplo di 3, non deve stampare il numero ma “PHP”;
+    Se multiplo di 5 deve stampare “JAVASCRIPT”;
+    Se multiplo di 3 e 5 (15) deve stampare “Bari”;*/
+
+
+for ($i = 1; $i <= 30; $i++) {
+    stampaQualcosa($i);
+}
+
+function isMultipleOfThree($number)
+{
+    return $number % 3 === 0;
+}
+
+function isMultipleOfFive($number)
+{
+    return $number % 5 === 0;
+}
+
+function isMultipleOfFifteen($number)
+{
+    return $number % 15 === 0;
+}
+
+function stampaQualcosa($number)
+{
+    if (isMultipleOfFifteen($number)) {
+        echo "Bari\n";
+    } elseif (isMultipleOfThree($number)) {
+        echo "PHP\n";
+    } elseif (isMultipleOfFive($number)) {
+        echo "JAVASCRIPT\n";
+    } else {
+        echo "$number\n";
+    }
+}
+
+//---- Esercizio 2 ----
+
+
+/*Dato il seguente array associativo:
+
+$persons = [
+  [
+    'name' => 'Marco',
+    'age' => 18,
+  ],
+  [
+    'name' => 'Vanessa',
+    'age' => 12,
+  ],
+  [
+    'name' => 'Jack',
+    'age' => 34,
+  ],
+  [
+    'name' => 'Maria',
+    'age' => 55,
+  ],
+];
+
+Scrivere un programma utilizzando le funzioni in grado di controllare l’ età  di ogni persona e stampare a video se:
+
+    Maggiore o uguale di 18: "$nome_persona può votare"
+    Minore di 18: "$nome_persona non può votare"*/
+
+$persons = [
+    [
+        'name' => 'Procopio',
+        'age' => 18,
+    ],
+    [
+        'name' => 'Adelaide',
+        'age' => 12,
+    ],
+    [
+        'name' => 'Flaviano',
+        'age' => 34,
+    ],
+    [
+        'name' => 'Adalgisa',
+        'age' => 55,
+    ],
+];
+
+$persons = [
+    [
+        'name' => 'Procopio',
+        'age' => 18,
+    ],
+    [
+        'name' => 'Adelaide',
+        'age' => 12,
+    ],
+    [
+        'name' => 'Flaviano',
+        'age' => 34,
+    ],
+    [
+        'name' => 'Adalgisa',
+        'age' => 55,
+    ],
+];
+
+function controllaEta($nome, $eta)
+{
+    if ($eta >= 18) {
+        echo "$nome può votare.\n";
+    } else {
+        echo "$nome non può votare.\n";
+    }
+}
+
+foreach ($persons as $persona) {
+    controllaEta($persona['name'], $persona['age']);
+}
+
+//---- Esercizio 3 ----
+/*Scrivere un programma funzionale che, dato un numero in input ($max), stampi a video:
+
+    PRIMA tutti i numeri Dispari
+    DOPO tutti i numeri PARI
+
+//Esempio max = 10
+1,3,5,7,9
+0,2,4,6,8,10*/
+
+function printNumbers($max)
+{
+
+    for ($i = 0; $i <= $max; $i++) {
+        if ($i % 2 !== 0) {
+            echo $i . ",";
+        }
+    }
+    echo "\n";
+
+    for ($i = 0; $i <= $max; $i++) {
+        if ($i % 2 === 0) {
+            echo $i . ",";
+        }
+    }
+    echo "\n";
+}
+
+$max = 10;
+printNumbers($max);
+
+//---- Esercizio 4 ----
+//Scrivere un programma che, data una stringa in input dal readline, conti quante vocali sono state inserite
+// Funzione per contare le vocali in una stringa
+
+
+function contaLettere($abc)
+{
+    $vocali = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    $x = 0;
+
+    for ($i = 0; $i < strlen($abc); $i++) {
+        if (in_array($abc[$i], $vocali)) {
+            $x++;
+        }
+    }
+    return $x;
+}
+
+echo "Scrivi qualcosa: ";
+$input = trim(fgets(STDIN)); 
+$vocali_trovate = contaLettere($input);
+echo "Numero di vocali in '$input': $vocali_trovate \n";
 
 
