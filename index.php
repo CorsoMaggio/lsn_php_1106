06461ae1ccd62a562914ba7b8cffd1726765aba2
Leysan@LAPTOP-25B9S5T6 <leisan.nasretdinova@gmail.com>
2025-06-11 09:36:41 +0200

    eserciziao5

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+// --- Esercizio 5---
+/*Utilizzando la logica appena appresa con l’operatore Modulo, scrivere un programma che stampi in console tutti i numeri da 1 a 30.
+
+    Se il numero e’ multiplo di 3 deve stampare “PHP”;
+    Se multiplo di 5 deve stampare “JAVASCRIPT”;
+    Se multiplo di 3 e 5 (15) deve stampare “Francesco”;*/
+
+//Risultato
+
+//1,2,PHP,4,Javascript,PHP,7,8,PHP,Javascript,11,PHP,13,14,Francesco,16,17,PHP,19,Javascript,PHP,22,23,PHP,Javascript,26,PHP,28,29,Francesco
+
+for ($i = 1; $i <= 30; $i++) {
+    if ($i % 3 === 0) {
+    }
+}
+echo "PHP \n"; 
+    else {
+  if ($i % 5 === 0) {
+    }
+}
+echo "JAVASCRIPT \n"; 
+    else {
+        if ($i % 15 === 0) {
+    }
+    echo "LEYSAN \n";
+    };
+    
+
+
+
+
+
+?>
