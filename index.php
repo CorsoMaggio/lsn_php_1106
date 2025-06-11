fcba89049bd473aded4dacc2dc0333b79d3cac3e
Leysan@LAPTOP-25B9S5T6 <leisan.nasretdinova@gmail.com>
2025-06-11 12:17:51 +0200

    esercizio 8

--- index.php
+++ index.php
@@ -59,3 +59,62 @@ for ($i = 0; $i < count($arrays); $i++) {
         echo "$nome ha preferito non specificare.\n";
     }
 }
+
+//---- Esercizio 7 ----
+//Ripetere il 6 con il foreach
+
+foreach ($arrays as $person) {
+    $nome = $person['name'];
+    $genere = $person['gender'];
+
+    if ($genere === 'M') {
+        echo "Buongiorno Sig. $nome\n";
+    } elseif ($genere === 'F') {
+        echo "Buongiorno Sig.ra $nome\n";
+    } else {
+        echo "$nome ha preferito non specificare.\n";
+    }
+}
+
+//---- Esercizio 8 ----
+/*Utilizzando il costrutto While (Pre-Condizionale), realizzare un sistema di "Aggiungi al carrello" in modo tale da eseguire delle operazioni di somma fino al raggiungimento del budget massimo inserito dall’utente appena eseguirete il programma: //Ad Esempio
+$budget = 10;// valore preso in input da readline
+//Aggiungo articolo 1
+$costo_articolo_1 = 5;// valore preso in input da readline
+$budget = $budget - costo_articolo_1;*/
+//Budget Rimanente 5 Euro;
+//Nel costrutto while, una volta definita la condizione, dovrete di volta in volta chiedere all’utente il costo dell’articolo e visualizzare a schermo il budget Rimanente. Nel momento in cui il budget sarà 0 il programma terminerà l'esecuzione. ATTENZIONE: Gestire l’eventualità in cui venga scritto un prezzo maggiore del budget a disposizione; 
+//Il Budget rimanente non potrà mai assumere un valore negativo;
+
+
+
+// Chiediamo il budget iniziale? senza readline 
+
+
+$budget = 10;
+$articoli = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+$articolo_num = 1;
+$i = 0;
+
+while ($budget > 0 && $i < count($articoli)) {
+    $costo = $articoli[$i];
+
+    echo " Aggiungi articolo $articolo_num (€$costo)\n";
+
+    if ($costo > $budget) {
+        echo "Il costo (€$costo) è maggiore del budget disponibile (€$budget)\n";
+    } else {
+        $budget -= $costo;
+        echo " Aggiunto. Budget rimanente: €$budget\n";
+        $articolo_num++;
+    }
+
+    $i++;
+}
+
+if ($budget === 0) {
+    echo "Budget esaurito.\n";
+} 
+
+
